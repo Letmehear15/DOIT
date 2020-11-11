f8f71eb1e7c6029a09fb2cf24b7cd5107cdaaddf
Anna Reshetova <anna.reshetovaM@gmail.com>
2020-11-11 16:39:40 +0100

    /articles GET Response: {     "articles": [         {             "id": ,             "name": ,             "autor": {                 "id": ,                 "login": ,                 "role": ,                 "name": ,                 "lastname": ,                 "email": ,                 "tel": "             },             "descr": ,             "comments": {} //TODO         }     ] }
    /logon GET
    Response:
    {
        "isAuth": true/false
        "role":
    }
    
    /signup
    Require: login, password - must have; role, name, lastname, email, tel
    Response: isCorrect(true/false), isSave(true/false)
    
    /articles/new
    Require: name, descr, autor(from Author)
    Response: isCorrect(true/false), isSave(true/false)

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -64,13 +64,19 @@ class ArticlesController extends AbstractController
         $errors = $validator->validate($article);
 
         if($errors){
+            $response->setData(['isCorrect' => true]);
+        }
+        else{
+            $response->setData(['isCorrect' => false]);
+        }
+        $entityManager->persist($article);
+        if($entityManager->flush()){
             $response->setData(['isSave' => true]);
         }
         else{
             $response->setData(['isSave' => false]);
         }
-        $entityManager->persist($article);
-        $entityManager->flush();
+
         return $response;
     }
 }
