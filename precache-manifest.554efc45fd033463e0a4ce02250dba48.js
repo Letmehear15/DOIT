1e87acf883540e60c5586804bc6405f2ffd2daa6
Aleksandr Nesterov <sanya.nesterov666@gmail.com>
2020-12-02 13:04:57 +0100

    Updates

---
+++ precache-manifest.554efc45fd033463e0a4ce02250dba48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d083ced7e244f58e6117fecd7dc4f04",
+    "url": "/DOIT/index.html"
+  },
+  {
+    "revision": "3343b12c7778959fa0fc",
+    "url": "/DOIT/static/css/main.5f777515.chunk.css"
+  },
+  {
+    "revision": "9aa11beb54178fc74c07",
+    "url": "/DOIT/static/js/2.837ce5c3.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/DOIT/static/js/2.837ce5c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3343b12c7778959fa0fc",
+    "url": "/DOIT/static/js/main.d91b52a1.chunk.js"
+  },
+  {
+    "revision": "e8906e81323c16944c22",
+    "url": "/DOIT/static/js/runtime-main.49c7ee35.js"
+  },
+  {
+    "revision": "47efbeaf837cf1e1cbd42f7aa1e99414",
+    "url": "/DOIT/static/media/img.47efbeaf.jpg"
+  }
+]);
