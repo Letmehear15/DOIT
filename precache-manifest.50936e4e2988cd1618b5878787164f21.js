ab64767714eadcb9c2ab8f3c911a1c3411ee9879
Aleksandr Nesterov <sanya.nesterov666@gmail.com>
2020-12-02 13:39:00 +0100

    Updates

---
+++ precache-manifest.50936e4e2988cd1618b5878787164f21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad73492ccc19f85836c5afb0d188211",
+    "url": "/DOIT/index.html"
+  },
+  {
+    "revision": "6e14069975b8c86abd81",
+    "url": "/DOIT/static/css/main.5f777515.chunk.css"
+  },
+  {
+    "revision": "9aa11beb54178fc74c07",
+    "url": "/DOIT/static/js/2.837ce5c3.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/DOIT/static/js/2.837ce5c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e14069975b8c86abd81",
+    "url": "/DOIT/static/js/main.88c59f6f.chunk.js"
+  },
+  {
+    "revision": "e8906e81323c16944c22",
+    "url": "/DOIT/static/js/runtime-main.49c7ee35.js"
+  },
+  {
+    "revision": "47efbeaf837cf1e1cbd42f7aa1e99414",
+    "url": "/DOIT/static/media/img.47efbeaf.jpg"
+  }
+]);
