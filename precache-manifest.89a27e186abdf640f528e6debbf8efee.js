00dbacbbfa191c28d8667cfc490df56ac3029462
Aleksandr Nesterov <sanya.nesterov666@gmail.com>
2020-12-11 19:57:00 +0100

    Updates

---
+++ precache-manifest.89a27e186abdf640f528e6debbf8efee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd5083c8fc90d754b4a518607db3ea5",
+    "url": "/DOIT/index.html"
+  },
+  {
+    "revision": "5e76cfbcacbb5081b57d",
+    "url": "/DOIT/static/css/main.5f777515.chunk.css"
+  },
+  {
+    "revision": "ffffedd3bebc40396104",
+    "url": "/DOIT/static/js/2.4771cc7d.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/DOIT/static/js/2.4771cc7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e76cfbcacbb5081b57d",
+    "url": "/DOIT/static/js/main.62be44ac.chunk.js"
+  },
+  {
+    "revision": "e8906e81323c16944c22",
+    "url": "/DOIT/static/js/runtime-main.49c7ee35.js"
+  },
+  {
+    "revision": "f3a53693fdbaf052e8f883c8fa2efcd6",
+    "url": "/DOIT/static/media/blog-post.1.f3a53693.md"
+  },
+  {
+    "revision": "99cbba3ba72c097ec32daa6f5eed33ec",
+    "url": "/DOIT/static/media/blog-post.2.99cbba3b.md"
+  },
+  {
+    "revision": "8d6e276be069ee011ba792969f01d327",
+    "url": "/DOIT/static/media/blog-post.3.8d6e276b.md"
+  },
+  {
+    "revision": "47efbeaf837cf1e1cbd42f7aa1e99414",
+    "url": "/DOIT/static/media/img.47efbeaf.jpg"
+  }
+]);
